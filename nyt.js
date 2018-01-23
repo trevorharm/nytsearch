ce5e7bc22fa6c7cd5384784b49a1c480dbd36d5e
wmgarvin <wmgarvin@gmail.com>
2018-01-22 21:47:34 -0500

    working test

--- nyt.js
+++ nyt.js
@@ -1,20 +1,22 @@
+$(document).ready(function () {
+    var apiKey = "0ff2f5d8344a49bcb162ef10a7a98a91";
 
-var apiKey = "0ff2f5d8344a49bcb162ef10a7a98a91";
+    $("#search").on("click", function () {
+        event.preventDefault();
 
-
-$("#search").on("click", function () {
-    var searchTerm = $("#query").val();
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-        'api-key': apiKey,
-        'q': searchTerm
-    });
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function (result) {
-        console.log(result.response.docs[0]);
-    }).fail(function (err) {
-        throw err;
+        var searchTerm = $("#query").val();
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+            'api-key': apiKey,
+            'q': searchTerm
+        });
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (result) {
+            console.log(result.response.docs[0]);
+        }).fail(function (err) {
+            throw err;
+        });
     });
 });
